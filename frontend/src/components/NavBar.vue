536d3f8a8592f06faf17928d169e675453d28409
Alexei <alakai20136@gmail.com>
2020-12-06 00:30:00 +0200

    api for frontend added. Front connected to backend

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="">TODO</a>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
